9358c004f89cad1e28d1cbaaa454f6fb97a4129b
Steven Vergenz <Steven.Vergenz@microsoft.com>
2018-02-13 13:59:18 -0800

    Hopefully fix dependencies on unused shaders

--- UnityGLTF/Assets/UnityGLTF/Scripts/Exceptions.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Exceptions.cs
@@ -13,4 +13,17 @@ namespace UnityGLTF {
 			System.Runtime.Serialization.StreamingContext context) { }
 #endif
 	}
+
+	[Serializable()]
+	public class ShaderNotFoundException : Exception
+	{
+		public ShaderNotFoundException() : base() { }
+		public ShaderNotFoundException(string message) : base(message) { }
+		public ShaderNotFoundException(string message, Exception inner) : base(message, inner) { }
+#if !WINDOWS_UWP
+		protected ShaderNotFoundException(System.Runtime.Serialization.SerializationInfo info,
+			System.Runtime.Serialization.StreamingContext context)
+		{ }
+#endif
+	}
 }
