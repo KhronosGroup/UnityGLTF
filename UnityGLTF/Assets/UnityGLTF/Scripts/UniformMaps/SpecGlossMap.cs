ff7fe55f55aedd6a94b02fdd4ae0b2cf43caf3bd
John Copic <jcopic@microsoft.com>
2018-03-26 15:03:31 -0700

    More spaces back to tabs.

--- UnityGLTF/Assets/UnityGLTF/Scripts/UniformMaps/SpecGlossMap.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/UniformMaps/SpecGlossMap.cs
@@ -3,47 +3,47 @@ using UnityEngine;
 
 namespace UnityGLTF
 {
-    class SpecGlossMap : SpecGloss2StandardMap
-    {
-        public SpecGlossMap(int MaxLOD = 1000) : base("GLTF/PbrSpecularGlossiness", MaxLOD) { }
-        protected SpecGlossMap(string shaderName, int MaxLOD = 1000) : base(shaderName, MaxLOD) { }
-        protected SpecGlossMap(Material m, int MaxLOD = 1000) : base(m, MaxLOD) { }
+	class SpecGlossMap : SpecGloss2StandardMap
+	{
+		public SpecGlossMap(int MaxLOD = 1000) : base("GLTF/PbrSpecularGlossiness", MaxLOD) { }
+		protected SpecGlossMap(string shaderName, int MaxLOD = 1000) : base(shaderName, MaxLOD) { }
+		protected SpecGlossMap(Material m, int MaxLOD = 1000) : base(m, MaxLOD) { }
 
-        public override int NormalTexCoord
-        {
-            get { return 0; }
-            set { return; }
-        }
+		public override int NormalTexCoord
+		{
+			get { return 0; }
+			set { return; }
+		}
 
-        public override int OcclusionTexCoord
-        {
-            get { return 0; }
-            set { return; }
-        }
+		public override int OcclusionTexCoord
+		{
+			get { return 0; }
+			set { return; }
+		}
 
-        public override int EmissiveTexCoord
-        {
-            get { return 0; }
-            set { return; }
-        }
+		public override int EmissiveTexCoord
+		{
+			get { return 0; }
+			set { return; }
+		}
 
-        public override int DiffuseTexCoord
-        {
-            get { return 0; }
-            set { return; }
-        }
+		public override int DiffuseTexCoord
+		{
+			get { return 0; }
+			set { return; }
+		}
 
-        public override int SpecularGlossinessTexCoord
-        {
-            get { return 0; }
-            set { return; }
-        }
+		public override int SpecularGlossinessTexCoord
+		{
+			get { return 0; }
+			set { return; }
+		}
 
-        public override IUniformMap Clone()
-        {
-            var copy = new SpecGlossMap(new Material(_material));
-            base.Copy(copy);
-            return copy;
-        }
-    }
+		public override IUniformMap Clone()
+		{
+			var copy = new SpecGlossMap(new Material(_material));
+			base.Copy(copy);
+			return copy;
+		}
+	}
 }
