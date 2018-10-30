fb04573e01cd1152947ac6eeaaeaac31daedbea4
Ahmed Zakaria (MSFT) <azakaria@microsoft.com>
2018-10-30 09:58:48 -0700

    Create Play mode tests to compare scene frame buffer output against master images (#273)
    * integrate GLTFAssetGeneratorTests
    
    This test generate master images using gltf files from glTF-Asset-Generator project and compares against master images
    
    * add glTF-Asset-Generator as a submodule
    
    * add scene tests
    
    and update animatedcube and lantern scenes to confoirm to the test
    
    * pr feedback

---
+++ UnityGLTF/Assets/Resources/Manifest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace AssetGenerator
+{
+    internal class Manifest
+    {
+        public string Folder;
+        public List<Model> Models = new List<Model>();
+
+        // Model group, to be listed in the manifest as the folder name
+        public Manifest()
+        {
+            
+        }
+
+        // Model properties to be listed in the manifest
+        public class Model
+        {
+            public string FileName;
+            [Newtonsoft.Json.JsonProperty( NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore )]
+            public string SampleImageName;
+            public Camera Camera;
+
+            public Model()
+            {
+                
+            }
+        }
+
+        // Camera properties
+        public class Camera
+        {
+            public float[] Translation = new float[3];
+
+            public Camera()
+            {
+                //cameratranslation.CopyTo(Translation);
+            }
+        }
+    }
+}
