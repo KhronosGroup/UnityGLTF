f49622074641d2a2c0e5b248134320f5f65a0bc1
Ahmed Zakaria <azakaria@microsoft.com>
2018-11-15 16:01:34 -0800

    msft_lod support for mesh lod

---
+++ GLTFSerialization/GLTFSerialization/Extensions/MSFT_LODExtension.cs
@@ -0,0 +1,31 @@
+﻿using GLTF.Math;
+using GLTF.Schema;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace GLTF.Schema
+{
+	/// <summary>
+	/// glTF extension that defines the LOD 
+	/// </summary>
+	public class MSFT_LODExtension : IExtension
+	{
+
+		public List<int> MeshIds { get; set; }
+		public MSFT_LODExtension(List<int> meshIds)
+		{
+			MeshIds = meshIds;
+		}
+		public IExtension Clone(GLTFRoot gltfRoot)
+		{
+			throw new NullReferenceException();
+		}
+		public JProperty Serialize()
+		{
+			throw new NullReferenceException();
+			//return new JProperty(ExtTextureTransformExtensionFactory.EXTENSION_NAME, ext);
+		}
+	}
+}
