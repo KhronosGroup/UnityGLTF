c963dc7fa69bfeeeaf27677dab77a3e1c3dc4975
Steven Vergenz <steven.vergenz@microsoft.com>
2019-01-29 16:44:59 -0800

    Fix LoadMaterialAsync for IL2CPP

--- UnityGLTF/Assets/UnityGLTF/Scripts/GLTFSceneImporter.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/GLTFSceneImporter.cs
@@ -293,8 +293,6 @@ namespace UnityGLTF
 					await ConstructMaterialImageBuffers(def);
 					await ConstructMaterial(def, materialIndex);
 				}
-
-				return _assetCache.MaterialCache[materialIndex].UnityMaterialWithVertexColor;
 			}
 			finally
 			{
@@ -303,6 +301,8 @@ namespace UnityGLTF
 					_isRunning = false;
 				}
 			}
+
+			return _assetCache.MaterialCache[materialIndex].UnityMaterialWithVertexColor;
 		}
 
 		/// <summary>
