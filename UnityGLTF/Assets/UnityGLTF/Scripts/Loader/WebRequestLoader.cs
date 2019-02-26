06e9f3680371485f1b8ec157e3cc780577cf7e77
Michele Kolodi <mykolodiy@gmail.com>
2019-02-26 09:31:11 +0100

    WebGL http download
    Using UnityWebRequest instead of .NET http client which fail on WebGL because of using nultithreading

--- UnityGLTF/Assets/UnityGLTF/Scripts/Loader/WebRequestLoader.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Loader/WebRequestLoader.cs
@@ -101,25 +101,28 @@ namespace UnityGLTF.Loader
 #if UNITY_WEBGL
 		private IEnumerator CreateHTTPRequest(string rootUri, string httpRequestPath)
 		{
-			UnityWebRequest www = new UnityWebRequest(Path.Combine(rootUri, httpRequestPath), "GET", new DownloadHandlerBuffer(), null);
-			www.timeout = 5000;
+
+			using (UnityWebRequest www = new UnityWebRequest(Path.Combine(rootUri, httpRequestPath), "GET", new DownloadHandlerBuffer(), null))
+			{
+				www.timeout = 5000;
 #if UNITY_2017_2_OR_NEWER
-			yield return www.SendWebRequest();
+				yield return www.SendWebRequest();
 #else
 			yield return www.Send();
 #endif
-			if ((int)www.responseCode >= 400)
-			{
-				Debug.LogErrorFormat("{0} - {1}", www.responseCode, www.url);
-				throw new Exception("Response code invalid");
-			}
+				if ((int)www.responseCode >= 400)
+				{
+					Debug.LogErrorFormat("{0} - {1}", www.responseCode, www.url);
+					throw new Exception("Response code invalid");
+				}
 
-			if (www.downloadedBytes > int.MaxValue)
-			{
-				throw new Exception("Stream is larger than can be copied into byte array");
-			}
+				if (www.downloadedBytes > int.MaxValue)
+				{
+					throw new Exception("Stream is larger than can be copied into byte array");
+				}
 
-			LoadedStream = new MemoryStream(www.downloadHandler.data, 0, www.downloadHandler.data.Length, true, true);
+				LoadedStream = new MemoryStream(www.downloadHandler.data, 0, www.downloadHandler.data.Length, true, true);
+			}
 		}
 #endif
 
