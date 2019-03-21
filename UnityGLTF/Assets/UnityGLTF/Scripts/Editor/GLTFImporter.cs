7c2cf34c89187a5519187fc9534bba349f021676
NORTHAMERICA\botri <botri@microsoft.com>
2019-03-21 16:20:41 -0700

    .

--- UnityGLTF/Assets/UnityGLTF/Scripts/Editor/GLTFImporter.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Editor/GLTFImporter.cs
@@ -340,7 +340,7 @@ namespace UnityGLTF
 				GLTFRoot gLTFRoot;
 				GLTFParser.ParseJson(stream, out gLTFRoot);
 				var loader = new GLTFSceneImporter(gLTFRoot, fileLoader, null, stream);
-				loader.BudgetPerFrameInMilliseconds = float.MaxValue;
+				loader.BudgetPerFrameInSeconds = float.MaxValue;
 				loader.MaximumLod = _maximumLod;
 				loader.IsMultithreaded = true;
 
