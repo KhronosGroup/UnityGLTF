de30766c7a8566d220aff442b863e8bbfc5475be
NORTHAMERICA\botri <botri@microsoft.com>
2019-03-21 13:23:42 -0700

    Yield timeout should be common to all loaders as framerate is an application-wide concern

--- UnityGLTF/Assets/UnityGLTF/Scripts/Async/AsyncCoroutineHelper.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Async/AsyncCoroutineHelper.cs
@@ -4,18 +4,22 @@ using System.Threading.Tasks;
 using UnityEngine;
 namespace UnityGLTF
 {
-    public interface IAsyncCoroutineHelper
-    {
-	   Task RunAsTask(IEnumerator coroutine, string name);
-    }
+	public interface IAsyncCoroutineHelper
+	{
+		Task RunAsTask(IEnumerator coroutine, string name);
+	}
 
 	public class AsyncCoroutineHelper : MonoBehaviour, IAsyncCoroutineHelper
-    {
+	{
+		public float BudgetPerFrameInSeconds = 0.01f;
+
 		private Queue<CoroutineInfo> _actions = new Queue<CoroutineInfo>();
+		private WaitForEndOfFrame _waitForEndOfFrame = new WaitForEndOfFrame();
+		private float _timeout;
 
 		public Task RunAsTask(IEnumerator coroutine, string name)
 		{
-			TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+			var tcs = new TaskCompletionSource<bool>();
 			lock (_actions)
 			{
 				_actions.Enqueue(
@@ -31,15 +35,24 @@ namespace UnityGLTF
 			return tcs.Task;
 		}
 
-		private IEnumerator CallMethodOnMainThread(CoroutineInfo coroutineInfo)
+		public async Task YieldOnTimeout()
 		{
-			yield return coroutineInfo.Coroutine;
-			coroutineInfo.Tcs.SetResult(true);
+			if (Time.realtimeSinceStartup > _timeout)
+			{
+				await RunAsTask(EmptyYieldEnum(), nameof(EmptyYieldEnum));
+			}
 		}
 
-	   private void Update()
+		private void Start()
 		{
-			CoroutineInfo? coroutineInfo = null;
+			_timeout = Time.realtimeSinceStartup + BudgetPerFrameInSeconds;
+		}
+
+		private void Update()
+		{
+			StartCoroutine(ResetFrameTimeout());
+
+			CoroutineInfo ? coroutineInfo = null;
 
 			lock (_actions)
 			{
@@ -53,6 +66,23 @@ namespace UnityGLTF
 			{
 				StartCoroutine(CallMethodOnMainThread(coroutineInfo.Value));
 			}
+		}
+
+		private IEnumerator CallMethodOnMainThread(CoroutineInfo coroutineInfo)
+		{
+			yield return coroutineInfo.Coroutine;
+			coroutineInfo.Tcs.SetResult(true);
+		}
+
+		private IEnumerator EmptyYieldEnum()
+		{
+			yield break;
+		}
+
+		private IEnumerator ResetFrameTimeout()
+		{
+			yield return _waitForEndOfFrame;
+			_timeout = Time.realtimeSinceStartup + BudgetPerFrameInSeconds;
 		}
 
 		private struct CoroutineInfo
