07d6a431aa2d995c5c63191221b5a5149241576f
Adam Mitchell <adammit@microsoft.com>
2019-04-29 15:10:40 -0700

    Add a component to display the list of sample models and let you load them at runtime.

---
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Tests/Editor/SampleModelListInspector.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityGLTF.Loader;
+
+[CustomEditor(typeof(SampleModelList))]
+public class SampleModelListInspector : Editor
+{
+	private List<SampleModel> models = null;
+	private bool requestedModelList = false;
+	private Vector2 scroll = Vector2.zero;
+
+	//private void OnEnable()
+	//{
+	//	Debug.Log("enable");
+	//}
+
+	//private void OnDisable()
+	//{
+	//	Debug.Log("disable");
+	//}
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUILayout.PropertyField(serializedObject.FindProperty(SampleModelList.LoaderFieldName));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty(SampleModelList.PathRootFieldName));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty(SampleModelList.ManifestRelativePathFieldName));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty(SampleModelList.ModelRelativePathFieldName));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty(SampleModelList.LoadThisFrameFieldName));
+
+		if (!Application.isPlaying)
+		{
+			models = null;
+			requestedModelList = false;
+			scroll = Vector2.zero;
+		}
+		else
+		{
+			if (!requestedModelList)
+			{
+				requestedModelList = true;
+
+				DownloadSampleModelList();
+			}
+
+
+			EditorGUILayout.LabelField("Models:");
+
+			if (models != null)
+			{
+				scroll = EditorGUILayout.BeginScrollView(scroll);
+
+				foreach (var model in models)
+				{
+					EditorGUILayout.BeginHorizontal();
+
+					EditorGUILayout.LabelField(model.Name);
+
+					foreach (var variant in model.Variants)
+					{
+						var buttonPressed = GUILayout.Button(variant.Type);
+
+						if (buttonPressed)
+						{
+							LoadModel(model.Name, variant.Type, variant.FileName);
+						}
+					}
+
+					EditorGUILayout.EndHorizontal();
+				}
+
+				EditorGUILayout.EndScrollView();
+			}
+		}
+	}
+
+	private void LoadModel(string modelName, string variantType, string variantName)
+	{
+		string relativePath = $"{modelName}/{variantType}/{variantName}";
+
+		serializedObject.FindProperty(SampleModelList.ModelRelativePathFieldName).stringValue = relativePath;
+		serializedObject.FindProperty(SampleModelList.LoadThisFrameFieldName).boolValue = true;
+	}
+
+	private async void DownloadSampleModelList()
+	{
+		var pathRoot = serializedObject.FindProperty(SampleModelList.PathRootFieldName).stringValue;
+		var manifestRelativePath = serializedObject.FindProperty(SampleModelList.ManifestRelativePathFieldName).stringValue;
+
+		var loader = new WebRequestLoader(pathRoot);
+		await loader.LoadStream(manifestRelativePath);
+
+		loader.LoadedStream.Seek(0, SeekOrigin.Begin);
+
+		var streamReader = new StreamReader(loader.LoadedStream);
+
+		//var s = streamReader.ReadToEnd();
+
+		var reader = new JsonTextReader(streamReader);
+		
+		reader.Read();
+		models = SampleModelListParser.ParseSampleModels(reader);
+	}
+}
---
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Tests/SampleModelList.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityGLTF;
+
+public class SampleModelList : MonoBehaviour
+{
+	public static string LoaderFieldName => nameof(loader);
+	public static string PathRootFieldName => nameof(pathRoot);
+	public static string ManifestRelativePathFieldName => nameof(manifestRelativePath);
+	public static string ModelRelativePathFieldName => nameof(modelRelativePath);
+	public static string LoadThisFrameFieldName => nameof(loadThisFrame);
+
+	[SerializeField]
+	private GLTFComponent loader = null;
+
+	[SerializeField]
+	private string pathRoot = "http://localhost:8080/glTF-Sample-Models/2.0/";
+
+	[SerializeField]
+	private string manifestRelativePath = "model-index.json";
+
+	[SerializeField]
+	private string modelRelativePath = null;
+
+	[SerializeField]
+	private bool loadThisFrame = false;
+
+	private async void Update()
+	{
+		if (loadThisFrame)
+		{
+			loadThisFrame = false;
+
+			var path = pathRoot + modelRelativePath;
+			loader.GLTFUri = path;
+			await loader.Load();
+		}
+	}
+}
