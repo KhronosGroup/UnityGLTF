702b4f149fc73735e688cb9900e2dd3c310c5c99
Adam Mitchell <adammit@microsoft.com>
2019-05-03 10:44:14 -0700

    right mouse camera controls

--- UnityGLTF/Assets/UnityGLTF/Examples/OrbitCameraController.cs
+++ UnityGLTF/Assets/UnityGLTF/Examples/OrbitCameraController.cs
@@ -3,10 +3,11 @@
 // Taken from http://wiki.unity3d.com/index.php?title=MouseOrbitImproved
 namespace UnityGLTF.Examples
 {
+	[RequireComponent(typeof(Camera))]
 	[AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
 	public class OrbitCameraController : MonoBehaviour
 	{
-		public Transform target;
+		public Vector3 targetPosition = Vector3.zero;
 		public float distance = 5.0f;
 		public float xSpeed = 120.0f;
 		public float ySpeed = 120.0f;
@@ -18,16 +19,20 @@ namespace UnityGLTF.Examples
 		public float distanceMin = .5f;
 		public float distanceMax = 150f;
 
+		private Camera camera;
+
 		private Rigidbody cameraRigidBody;
 
-		float x = 0.0f;
-		float y = 0.0f;
+		private float x = 0.0f;
+		private float y = 0.0f;
 
 		Quaternion rotation;
 
 		// Use this for initialization
 		void Start()
 		{
+			camera = GetComponent<Camera>();
+
 			Vector3 angles = transform.eulerAngles;
 			x = angles.y;
 			y = angles.x;
@@ -44,38 +49,47 @@ namespace UnityGLTF.Examples
 
 		void LateUpdate()
 		{
-			if (target)
+			var height = Display.main.renderingHeight;
+			var width = Display.main.renderingWidth;
+
+			if (Input.GetMouseButton(0))
 			{
-				if (Input.GetMouseButton(0))
-				{
-					x += Input.GetAxis("Mouse X") * xSpeed * 0.06f;
-					y -= Input.GetAxis("Mouse Y") * ySpeed * 0.06f;
+				x += Input.GetAxis("Mouse X") * xSpeed * 0.06f;
+				y -= Input.GetAxis("Mouse Y") * ySpeed * 0.06f;
 
-					y = ClampAngle(y, yMinLimit, yMaxLimit);
+				y = ClampAngle(y, yMinLimit, yMaxLimit);
 
-					rotation = Quaternion.Euler(y, x, 0);
-				}
-
-				var height = Display.main.renderingHeight;
-				var width = Display.main.renderingWidth;
-
-				var mouseOverRenderArea =
-					Input.mousePosition.x >= 0 &&
-					Input.mousePosition.x <= width &&
-					Input.mousePosition.y >= 0 &&
-					Input.mousePosition.y <= height;
-
-				if (Input.GetMouseButton(0) || mouseOverRenderArea)
-				{
-					distance = Mathf.Clamp(distance * Mathf.Exp(-Input.GetAxis("Mouse ScrollWheel") * zoomSpeed), distanceMin, distanceMax);
-				}
-
-				Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-				Vector3 position = rotation * negDistance + target.position;
-
-				transform.rotation = rotation;
-				transform.position = position;
+				rotation = Quaternion.Euler(y, x, 0);
 			}
+			else if (Input.GetMouseButton(1))
+			{
+				var distancePerPixel = 4.0f * distance * Mathf.Tan(camera.fieldOfView / 2.0f) / height;
+
+				float deltaX = Input.GetAxis("Mouse X") * distancePerPixel;
+				float deltaY = Input.GetAxis("Mouse Y") * distancePerPixel;
+
+				Vector3 deltaRight = transform.right * deltaX;
+				Vector3 deltaUp = transform.up * deltaY;
+
+				targetPosition += deltaRight + deltaUp;
+			}
+
+			var mouseOverRenderArea =
+				Input.mousePosition.x >= 0 &&
+				Input.mousePosition.x <= width &&
+				Input.mousePosition.y >= 0 &&
+				Input.mousePosition.y <= height;
+
+			if (Input.GetMouseButton(0) || mouseOverRenderArea)
+			{
+				distance = Mathf.Clamp(distance * Mathf.Exp(-Input.GetAxis("Mouse ScrollWheel") * zoomSpeed), distanceMin, distanceMax);
+			}
+
+			Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+			Vector3 position = rotation * negDistance + targetPosition;
+
+			transform.rotation = rotation;
+			transform.position = position;
 		}
 
 		public static float ClampAngle(float angle, float min, float max)
