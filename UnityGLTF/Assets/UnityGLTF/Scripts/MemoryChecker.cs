557b05c4fd68edd90e9ba33bf32965ac8ecf3576
Adam Mitchell <adammit@microsoft.com>
2019-06-20 09:00:45 -0700

    Add windows using.  Comments.  Tabs.

--- UnityGLTF/Assets/UnityGLTF/Scripts/MemoryChecker.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/MemoryChecker.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using UnityEngine;
+#if WINDOWS_UWP
+using Windows.System;
+#endif
 
 namespace UnityGLTF
 {
@@ -7,19 +10,25 @@ namespace UnityGLTF
 	{
 		private bool outOfMemory = false;
 
+		/// <summary>
+		/// Allows polling for app low memory situation.  Listens to Application.lowMemory, which works for iOS and Android.
+		/// Also listens to Windows-specific MemoryManager.AppMemoryUsageIncreased.
+		/// </summary>
 		public MemoryChecker()
 		{
 			Application.lowMemory += Application_lowMemory;
 
 #if WINDOWS_UWP
-        MemoryManager.AppMemoryUsageIncreased += MemoryManager_AppMemoryUsageIncreased;
+			MemoryManager.AppMemoryUsageIncreased += MemoryManager_AppMemoryUsageIncreased;
 #endif
 		}
 
+		/// <summary>
+		/// If the OS has notified the app that it is low on memory since MemoryChecker was constructed, this method will throw an OutOfMemoryException.
+		/// Once it throws once, it will continue to throw.  Callers are advised to construct a new MemoryChecker after memory usage has gone down.
+		/// </summary>
 		public void ThrowIfOutOfMemory()
 		{
-			//TODO: when should we reset outOfMemory to false?
-
 			if (outOfMemory)
 			{
 				throw new OutOfMemoryException();
@@ -32,13 +41,13 @@ namespace UnityGLTF
 		}
 
 #if WINDOWS_UWP
-    private void MemoryManager_AppMemoryUsageIncreased(object sender, object e)
-    {
-        if (MemoryManager.AppMemoryUsageLevel == AppMemoryUsageLevel.OverLimit)
-        {
-            outOfMemory = true;
-        }
-    }
+		private void MemoryManager_AppMemoryUsageIncreased(object sender, object e)
+		{
+			if (MemoryManager.AppMemoryUsageLevel == AppMemoryUsageLevel.OverLimit)
+			{
+				outOfMemory = true;
+			}
+		}
 #endif
 	}
 }
