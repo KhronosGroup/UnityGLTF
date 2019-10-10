d6dd92160baa42e66a0ac177d8691b9b895fbac1
Felix Herbst <herbst@prefrontalcortex.de>
2019-10-10 22:25:46 +0200

    change spaces to tabs

--- GLTFSerialization/GLTFSerialization/Exceptions.cs
+++ GLTFSerialization/GLTFSerialization/Exceptions.cs
@@ -26,15 +26,15 @@ namespace GLTF
 #endif
 	}
 
-    public class GLTFLoadException : Exception
-    {
-        public GLTFLoadException() : base() { }
-        public GLTFLoadException(string message) : base(message) { }
-        public GLTFLoadException(string message, Exception inner) : base(message, inner) { }
+	public class GLTFLoadException : Exception
+	{
+		public GLTFLoadException() : base() { }
+		public GLTFLoadException(string message) : base(message) { }
+		public GLTFLoadException(string message, Exception inner) : base(message, inner) { }
 #if !WINDOWS_UWP
-        protected GLTFLoadException(System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context)
-        { }
+		protected GLTFLoadException(System.Runtime.Serialization.SerializationInfo info,
+			System.Runtime.Serialization.StreamingContext context)
+		{ }
 #endif
-    }
+	}
 }
