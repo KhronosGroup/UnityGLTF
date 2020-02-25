2321316d76b9ef0a658c34f8e1fbda5b2fad8525
Felix Herbst <herbst@prefrontalcortex.de>
2020-02-25 13:20:58 +0100

    proper settings dirty mark

--- UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/GLTFSettings.cs
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/GLTFSettings.cs
@@ -8,13 +8,88 @@ namespace UnityGLTF
 	{
 		public const string k_MyCustomSettingsPath = "Assets/GLTFSettings_dontmove.asset";
 
-		public bool ExportNames = true;
-		public bool ExportFullPath = true;
-		public bool RequireExtensions = false;
-		public bool TryExportTexturesFromDisk = true;
-		public bool ExportAnimations = true;
-		public bool BakeSkinnedMeshes = false;
-		public string SaveFolderPath = "";
+		[SerializeField]
+		public bool exportNames = true;
+		[SerializeField]
+		public bool exportFullPath = true;
+		[SerializeField]
+		public bool requireExtensions = false;
+		[SerializeField]
+		public bool tryExportTexturesFromDisk = true;
+		[SerializeField]
+		public bool exportAnimations = true;
+		[SerializeField]
+		public bool bakeSkinnedMeshes = false;
+		[SerializeField]
+		public string saveFolderPath = "";
+
+		public bool ExportNames { get => exportNames;
+			set {
+				if(exportNames != value) {
+					exportNames = value;
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
+		
+		public bool ExportFullPath
+		{ get => exportFullPath;
+			set {
+				if(exportFullPath != value) {
+					exportFullPath = value;
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
+		public bool RequireExtensions
+		{ get => requireExtensions;
+			set {
+				if(requireExtensions != value) {
+					requireExtensions = value;
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
+		
+		public bool TryExportTexturesFromDisk
+		{ get => tryExportTexturesFromDisk;
+			set {
+				if(tryExportTexturesFromDisk != value) {
+					tryExportTexturesFromDisk = value;
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
+		
+		public bool ExportAnimations
+		{ get => exportAnimations;
+			set {
+				if(exportAnimations != value) {
+					exportAnimations = value;
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
+		
+		public bool BakeSkinnedMeshes
+		{ get => bakeSkinnedMeshes;
+			set {
+				if(bakeSkinnedMeshes != value) {
+					bakeSkinnedMeshes = value;
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
+		
+		public string SaveFolderPath
+		{ get => saveFolderPath;
+			set {
+				if(saveFolderPath != value) {
+					saveFolderPath = value;
+					EditorUtility.SetDirty(this);
+				}
+			}
+		}
 
 		internal static GLTFSettings GetOrCreateSettings()
 		{
