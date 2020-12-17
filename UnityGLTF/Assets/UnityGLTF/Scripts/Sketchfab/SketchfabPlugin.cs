52c6e8b58d7a67795dfc2af0fe4e3a63b9f79caa
Aurélien Chatelain <chatelain.aurelien@gmail.com>
2020-12-17 16:06:07 +0100

    Updates plugin version to 1.2.0
    * Refresh UI and plugin logic
    * Make store purchases available in plugin
    * Add a search filter to filter by license
    * If any license.txt in archive, copy it next to imported
    model prefab
    * Add support for PLUS account in plugin

--- UnityGLTF/Assets/UnityGLTF/Scripts/Sketchfab/SketchfabPlugin.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Sketchfab/SketchfabPlugin.cs
@@ -12,7 +12,7 @@ namespace Sketchfab
 {
 	public class SketchfabPlugin : MonoBehaviour
 	{
-		public static string VERSION = "1.1.1";
+		public static string VERSION = "1.2.0";
 
 		public struct Urls
 		{
