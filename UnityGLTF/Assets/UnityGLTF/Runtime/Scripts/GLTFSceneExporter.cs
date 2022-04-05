c0c6f4063561ea85e538aa72f8d1084d4ce0f408
Marcel Wiessler <marcel@gaisterhand.de>
2022-04-05 18:54:27 +0200

    Fix: remove duplicate AfterSceneExport event

--- UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/GLTFSceneExporter.cs
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/GLTFSceneExporter.cs
@@ -347,12 +347,6 @@ namespace UnityGLTF
 			{
 				ExportImages(path);
 			}
-
-			if (_exportOptions.AfterSceneExport != null)
-				_exportOptions.AfterSceneExport(this, _root);
-
-			if (AfterSceneExport != null)
-				AfterSceneExport.Invoke(this, _root);
 		}
 
 		/// <summary>
