d7c01ea57e99f608f3c2d229dffd0db21703b820
Felix Herbst <herbst@prefrontalcortex.de>
2022-04-25 12:59:21 +0200

    added basic UnityWebRequest loader to simplify accessing files and urls

---
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/Loader/UnityWebRequestLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace UnityGLTF.Loader
+{
+	public class UnityWebRequestLoader : IDataLoader
+	{
+		private string dir;
+
+		public UnityWebRequestLoader(string dir)
+		{
+			this.dir = dir;
+		}
+
+		public async Task<Stream> LoadStreamAsync(string relativeFilePath)
+		{
+			var path = Path.Combine(dir, relativeFilePath);
+			var request = UnityWebRequest.Get(path);
+			var asyncOperation = request.SendWebRequest();
+
+			while (!asyncOperation.isDone) {
+				await Task.Yield();
+			}
+
+			var results = request.downloadHandler.data;
+			Debug.Log("got stream, length " + results.Length);
+			var stream = new MemoryStream(results, 0, results.Length, false, true);
+			return stream;
+		}
+	}
+}
