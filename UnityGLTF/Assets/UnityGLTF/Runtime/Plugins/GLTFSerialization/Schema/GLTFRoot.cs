1f064871920251b4d757c28505efc06e431d32e1
Felix Herbst <herbst@prefrontalcortex.de>
2022-04-27 12:16:32 +0200

    change: rearrange JSON serialization order to have asset and scene data at the top

--- UnityGLTF/Assets/UnityGLTF/Runtime/Plugins/GLTFSerialization/Schema/GLTFRoot.cs
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Plugins/GLTFSerialization/Schema/GLTFRoot.cs
@@ -372,6 +372,9 @@ namespace GLTF.Schema
 
 			jsonWriter.WriteStartObject();
 
+			jsonWriter.WritePropertyName("asset");
+			Asset.Serialize(jsonWriter);
+
 			if (ExtensionsUsed != null && ExtensionsUsed.Count > 0)
 			{
 				jsonWriter.WritePropertyName("extensionsUsed");
@@ -390,6 +393,23 @@ namespace GLTF.Schema
 				foreach (var extension in ExtensionsRequired)
 				{
 					jsonWriter.WriteValue(extension);
+				}
+				jsonWriter.WriteEndArray();
+			}
+
+			if (Scene != null)
+			{
+				jsonWriter.WritePropertyName("scene");
+				Scene.Serialize(jsonWriter);
+			}
+
+			if (Scenes != null && Scenes.Count > 0)
+			{
+				jsonWriter.WritePropertyName("scenes");
+				jsonWriter.WriteStartArray();
+				foreach (var scene in Scenes)
+				{
+					scene.Serialize(jsonWriter);
 				}
 				jsonWriter.WriteEndArray();
 			}
@@ -415,9 +435,6 @@ namespace GLTF.Schema
 				}
 				jsonWriter.WriteEndArray();
 			}
-
-			jsonWriter.WritePropertyName("asset");
-			Asset.Serialize(jsonWriter);
 
 			if (Buffers != null && Buffers.Count > 0)
 			{
@@ -509,23 +526,6 @@ namespace GLTF.Schema
 				foreach (var sampler in Samplers)
 				{
 					sampler.Serialize(jsonWriter);
-				}
-				jsonWriter.WriteEndArray();
-			}
-
-			if (Scene != null)
-			{
-				jsonWriter.WritePropertyName("scene");
-				Scene.Serialize(jsonWriter);
-			}
-
-			if (Scenes != null && Scenes.Count > 0)
-			{
-				jsonWriter.WritePropertyName("scenes");
-				jsonWriter.WriteStartArray();
-				foreach (var scene in Scenes)
-				{
-					scene.Serialize(jsonWriter);
 				}
 				jsonWriter.WriteEndArray();
 			}
