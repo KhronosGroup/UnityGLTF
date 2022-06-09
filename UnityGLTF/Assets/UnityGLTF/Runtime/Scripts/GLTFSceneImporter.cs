882656ae2e0ce8646c2204837eeac680f5b08a0d
Felix Herbst <herbst@prefrontalcortex.de>
2022-06-09 02:13:48 +0200

    move internal block down

--- UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/GLTFSceneImporter.cs
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/GLTFSceneImporter.cs
@@ -523,30 +523,6 @@ namespace UnityGLTF
 			}
 		}
 
-#if UNITY_EDITOR
-		internal class AssetDatabaseStream : Stream
-		{
-			public string AssetUri { get; }
-
-			public AssetDatabaseStream(string imageUri)
-			{
-				AssetUri = imageUri;
-			}
-
-			public override void Flush() => throw new NotImplementedException();
-			public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
-			public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
-			public override void SetLength(long value) => throw new NotImplementedException();
-			public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
-
-			public override bool CanRead { get; }
-			public override bool CanSeek { get; }
-			public override bool CanWrite { get; }
-			public override long Length { get; }
-			public override long Position { get; set; }
-		}
-#endif
-
 		protected async Task ConstructImageBuffer(GLTFTexture texture, int textureIndex)
 		{
 			int sourceId = GetTextureSourceId(texture);
@@ -3023,5 +2999,29 @@ namespace UnityGLTF
 				}
 			}
 		}
+
+#if UNITY_EDITOR
+		internal class AssetDatabaseStream : Stream
+		{
+			public string AssetUri { get; }
+
+			public AssetDatabaseStream(string imageUri)
+			{
+				AssetUri = imageUri;
+			}
+
+			public override void Flush() => throw new NotImplementedException();
+			public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+			public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
+			public override void SetLength(long value) => throw new NotImplementedException();
+			public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+
+			public override bool CanRead { get; }
+			public override bool CanSeek { get; }
+			public override bool CanWrite { get; }
+			public override long Length { get; }
+			public override long Position { get; set; }
+		}
+#endif
 	}
 }
