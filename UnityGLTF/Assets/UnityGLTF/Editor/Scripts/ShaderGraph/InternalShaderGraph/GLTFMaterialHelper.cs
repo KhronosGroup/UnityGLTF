dddc1c3ad17960b5167f5459b8e453562dd70560
Felix Herbst <herbst@prefrontalcortex.de>
2022-07-01 22:02:32 +0200

    fix: PBRGraph wasn't assignable via material inspector if source shader wasn't known
    feat: ability to add conversion callbacks for material inspector shader assignments

--- UnityGLTF/Assets/UnityGLTF/Editor/Scripts/ShaderGraph/InternalShaderGraph/GLTFMaterialHelper.cs
+++ UnityGLTF/Assets/UnityGLTF/Editor/Scripts/ShaderGraph/InternalShaderGraph/GLTFMaterialHelper.cs
@@ -9,75 +9,103 @@ namespace UnityGLTF
 {
 	public static class GLTFMaterialHelper
 	{
-		public static void ConvertMaterialToGLTF(Material material, Shader oldShader, Shader newShader)
+		/// <summary>
+		/// Return false if other delegates should take over. Only return true if you did work and assigned material.shader = newShader.
+		/// </summary>
+		public delegate bool ConvertMaterialToGLTFDelegate(Material material, Shader oldShader, Shader newShader);
+		private static event ConvertMaterialToGLTFDelegate ConvertMaterialDelegates;
+
+		public static void RegisterMaterialConversionToGLTF(ConvertMaterialToGLTFDelegate converter) => ConvertMaterialDelegates += converter;
+		public static void UnregisterMaterialConversionToGLTF(ConvertMaterialToGLTFDelegate converter) => ConvertMaterialDelegates -= converter;
+
+		static GLTFMaterialHelper()
 		{
-			// TODO ideally this would use the same code path as material export/import - would reduce the amount of code duplication considerably.
-			// E.g. calling something like
-			// var glTFMaterial = ExportMaterial(material);
-			// ImportAndOverrideMaterial(material, glTFMaterial);
-			// that uses all the same heuristics, texture conversions, ...
+			RegisterMaterialConversionToGLTF(ConvertStandardAndURPLit);
+		}
 
-			// Debug.Log("New shader: " + newShader + ", old shader: " + oldShader);
-
+		private static bool ConvertStandardAndURPLit(Material material, Shader oldShader, Shader newShader)
+		{
 			// Conversion time!
 			// convert from
 			// - "Standard"
 			// - "URP/Lit"
 
-			if (oldShader.name == StandardShader || oldShader.name == URPLitShader)
+			if (oldShader.name != StandardShader && oldShader.name != URPLitShader) return false;
+
+			var isStandard = oldShader.name == StandardShader;
+			var needsEmissiveColorSpaceConversion = isStandard && QualitySettings.activeColorSpace == ColorSpace.Linear;
+			var colorProp = oldShader.name == StandardShader ? _Color : _BaseColor;
+			var colorTexProp = oldShader.name == StandardShader ? _MainTex : _BaseMap;
+
+			var color = material.GetColor(colorProp);
+			var albedo = material.GetTexture(colorTexProp);
+			var albedoOffset = material.GetTextureOffset(colorTexProp);
+			var albedoTiling = material.GetTextureScale(colorTexProp);
+
+			var metallic = material.GetFloat(_Metallic);
+			var smoothness = material.HasProperty(_Smoothness) ? material.GetFloat(_Smoothness) : material.GetFloat(_Glossiness);
+			var metallicGloss = material.GetTexture(_MetallicGlossMap);
+			var normal = material.GetTexture(_BumpMap);
+			var normalStrength = material.GetFloat(_BumpScale);
+			var occlusion = material.GetTexture(_OcclusionMap);
+			var occlusionStrength = material.GetFloat(_Strength);
+			var emission = material.GetTexture(_EmissionMap);
+			var emissionColor = material.GetColor(_EmissionColor);
+			var cutoff = material.GetFloat(_Cutoff);
+
+			material.shader = newShader;
+
+			material.SetColor(baseColorFactor, color);
+			material.SetTexture(baseColorTexture, albedo);
+			material.SetTextureOffset(baseColorTexture, albedoOffset);
+			material.SetTextureScale(baseColorTexture, albedoTiling);
+			if (albedoOffset != Vector2.zero || albedoTiling != Vector2.one)
+				material.SetKeyword("_TRANSMISSION_VOLUME", true);
+
+			material.SetFloat(metallicFactor, metallic);
+			material.SetFloat(roughnessFactor, 1 - smoothness);
+			// TODO: convert metallicGloss to metallicRoughnessTexture format: Metallic (R) + Smoothness (A) → Roughness (G) + Metallic (B)
+			// TODO: when smoothness is not 0 or 1 and there's a texture, need to convert to a texture and set roughness = 1, otherwise the math doesn't match
+			// TODO: figure out where to put the newly created texture, and how to avoid re-creating it several times when multiple materials may use it.
+			material.SetTexture(metallicRoughnessTexture, metallicGloss);
+			material.SetTexture(normalTexture, normal);
+			material.SetFloat(normalScale, normalStrength);
+			material.SetTexture(occlusionTexture, occlusion);
+			material.SetFloat(occlusionStrength1, occlusionStrength);
+			material.SetTexture(emissiveTexture, emission);
+			material.SetFloat(alphaCutoff, cutoff);
+
+			material.SetColor(emissiveFactor, needsEmissiveColorSpaceConversion ? emissionColor.linear : emissionColor);
+
+			// ensure keywords are correctly set after conversion
+			ShaderGraphHelpers.ValidateMaterialKeywords(material);
+
+			return true;
+		}
+
+		public static void ConvertMaterialToGLTF(Material material, Shader oldShader, Shader newShader)
+		{
+			if (ConvertMaterialDelegates != null)
 			{
-				var isStandard = oldShader.name == StandardShader;
-				var needsEmissiveColorSpaceConversion = isStandard && QualitySettings.activeColorSpace == ColorSpace.Linear;
-				var colorProp = oldShader.name == StandardShader ? _Color : _BaseColor;
-				var colorTexProp = oldShader.name == StandardShader ? _MainTex : _BaseMap;
-
-				var color = material.GetColor(colorProp);
-				var albedo = material.GetTexture(colorTexProp);
-				var albedoOffset = material.GetTextureOffset(colorTexProp);
-				var albedoTiling = material.GetTextureScale(colorTexProp);
-
-				var metallic = material.GetFloat(_Metallic);
-				var smoothness = material.HasProperty(_Smoothness) ? material.GetFloat(_Smoothness) : material.GetFloat(_Glossiness);
-				var metallicGloss = material.GetTexture(_MetallicGlossMap);
-				var normal = material.GetTexture(_BumpMap);
-				var normalStrength = material.GetFloat(_BumpScale);
-				var occlusion = material.GetTexture(_OcclusionMap);
-				var occlusionStrength = material.GetFloat(_Strength);
-				var emission = material.GetTexture(_EmissionMap);
-				var emissionColor = material.GetColor(_EmissionColor);
-				var cutoff = material.GetFloat(_Cutoff);
-
-				material.shader = newShader;
-
-				material.SetColor(baseColorFactor, color);
-				material.SetTexture(baseColorTexture, albedo);
-				material.SetTextureOffset(baseColorTexture, albedoOffset);
-				material.SetTextureScale(baseColorTexture, albedoTiling);
-				if (albedoOffset != Vector2.zero || albedoTiling != Vector2.one)
-					material.SetKeyword("_TRANSMISSION_VOLUME", true);
-
-				material.SetFloat(metallicFactor, metallic);
-				material.SetFloat(roughnessFactor, 1 - smoothness);
-				// TODO: convert metallicGloss to metallicRoughnessTexture format: Metallic (R) + Smoothness (A) → Roughness (G) + Metallic (B)
-				// TODO: when smoothness is not 0 or 1 and there's a texture, need to convert to a texture and set roughness = 1, otherwise the math doesn't match
-				// TODO: figure out where to put the newly created texture, and how to avoid re-creating it several times when multiple materials may use it.
-				material.SetTexture(metallicRoughnessTexture, metallicGloss);
-				material.SetTexture(normalTexture, normal);
-				material.SetFloat(normalScale, normalStrength);
-				material.SetTexture(occlusionTexture, occlusion);
-				material.SetFloat(occlusionStrength1, occlusionStrength);
-				material.SetTexture(emissiveTexture, emission);
-				material.SetFloat(alphaCutoff, cutoff);
-
-				material.SetColor(emissiveFactor, needsEmissiveColorSpaceConversion ? emissionColor.linear : emissionColor);
-
-				// ensure keywords are correctly set after conversion
-				ShaderGraphHelpers.ValidateMaterialKeywords(material);
+				var list = ConvertMaterialDelegates.GetInvocationList();
+				foreach (var entry in list)
+				{
+					var cb = (ConvertMaterialToGLTFDelegate) entry;
+					if (cb != null && cb.Invoke(material, oldShader, newShader))
+					{
+						return;
+					}
+				}
 			}
-			else
-			{
-				Debug.Log("No automatic conversion from " + oldShader.name + " to " + newShader.name + " found. Make sure your material properties match the new shader. If you think this should have been converted automatically: please report a bug!");
-			}
+
+			// IDEA ideally this would use the same code path as material export/import - would reduce the amount of code duplication considerably.
+			// E.g. calling something like
+			// var glTFMaterial = ExportMaterial(material);
+			// ImportAndOverrideMaterial(material, glTFMaterial);
+			// that uses all the same heuristics, texture conversions, ...
+
+			Debug.Log("No automatic conversion from " + oldShader.name + " to " + newShader.name + " found. Make sure your material properties match the new shader. You can add your own conversion callbacks via `RegisterMaterialConversionToGLTF`. If you think this should have been converted automatically: please open a feature request!");
+			material.shader = newShader;
 		}
 
 		private static void SetKeyword(this Material material, string keyword, bool state)
