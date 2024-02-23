234f5354e6f96b988d8215935ac3538e9da91df6
pfcDorn <dorn@prefrontalcortex.de>
2024-02-23 12:59:42 +0100

    added gpu instancing plugin import context

---
+++ Runtime/Scripts/Plugins/GPUInstancingImport.cs
@@ -0,0 +1,17 @@
+﻿namespace UnityGLTF.Plugins
+{
+    public class GPUInstancingImport: GLTFImportPlugin
+    {
+        public override string DisplayName => "EXT_mesh_gpu_instancing";
+        public override string Description => "Imports GPU instancing.";
+        public override GLTFImportPluginContext CreateInstance(GLTFImportContext context)
+        {
+            return new GPUInstancingImportContext();
+        }
+    }
+    
+    public class GPUInstancingImportContext: GLTFImportPluginContext
+    {
+        
+    }
+}
