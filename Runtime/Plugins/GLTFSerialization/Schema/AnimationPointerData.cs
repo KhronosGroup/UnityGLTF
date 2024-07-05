dcacd833320eab456dbf23d9406ff1c91e5022a5
Robert Dorn <dorn@prefrontalcortex.de>
2024-07-05 09:06:28 +0200

    fixes #740 - added missing ExtTransform for some textures in animationpointer import/export, fixes when only offset exist

--- Runtime/Plugins/GLTFSerialization/Schema/AnimationPointerData.cs
+++ Runtime/Plugins/GLTFSerialization/Schema/AnimationPointerData.cs
@@ -12,9 +12,12 @@ namespace GLTF.Schema
         public ImportValuesConversion importAccessorContentConversion;
 
         public string primaryPath = "";
+        public string primaryProperty = "";
+        
         public AttributeAccessor primaryData;
         
         public string secondaryPath = "";
+        public string secondaryProperty = "";
         public AttributeAccessor secondaryData;
     }
 
