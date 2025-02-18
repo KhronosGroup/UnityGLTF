dcf7e9bce24bf38a05fc563fb58a3a23fb198fd4
Robert Dorn <dorn@prefrontalcortex.de>
2025-02-18 11:46:54 +0100

    added UnityGltf Package Version, Unity Version and current Render Pipeline to gltf generator export

---
+++ Editor/Scripts/PackageVersionPreprocessBuild.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace UnityGLTF
+{
+     public class PackageVersionPreprocessBuild : IPreprocessBuildWithReport
+     {
+         public int callbackOrder
+         {
+             get => 0;
+         }
+         
+         private static void SetPackageVersion(bool resetValue = false)
+         {
+             var defaultSettings = GLTFSettings.GetOrCreateSettings();
+             if (!defaultSettings)
+               return;
+
+             defaultSettings.packageVersion = resetValue ? null : defaultSettings.GetUnityGltfVersion();
+             EditorUtility.SetDirty(defaultSettings);
+             AssetDatabase.SaveAssets();
+             AssetDatabase.Refresh();
+         }
+         
+         public void OnPreprocessBuild(BuildReport report)
+         {
+             SetPackageVersion();
+         }
+         
+         [PostProcessBuild(1)]
+         public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+         {
+             SetPackageVersion(true);
+         }
+    }
+}
