8e0b9946297928baae94f72a34463192fc3989ec
Robert Dorn <dorn@prefrontalcortex.de>
2025-03-12 19:42:33 +0100

    updated Babylon Log node > op is now "flow/log"

--- Runtime/Scripts/Interactivity/Schema/Nodes/Babylon_LogNode.cs
+++ Runtime/Scripts/Interactivity/Schema/Nodes/Babylon_LogNode.cs
@@ -2,10 +2,10 @@ namespace UnityGLTF.Interactivity.VisualScripting.Schema
 {
     public class Babylon_LogNode: GltfInteractivityNodeSchema
     {
-        public override string Op { get; set; } = "babylon/log";
+        public override string Op { get; set; } = "flow/log";
         public override string Extension { get; protected set; } = EXTENSION_ID; 
 
-        public const string EXTENSION_ID = "babylon/log";
+        public const string EXTENSION_ID = "BABYLON";
 
         [FlowInSocketDescription]
         public const string IdFlowIn = "in";
