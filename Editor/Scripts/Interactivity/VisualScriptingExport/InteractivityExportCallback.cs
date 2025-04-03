7174c0ecbbe1f9fd42b9c37d2b70c7bcd29baa63
Robert Dorn <dorn@prefrontalcortex.de>
2025-04-03 09:16:20 +0200

    added AddLog to export API

--- Editor/Scripts/Interactivity/VisualScriptingExport/InteractivityExportCallback.cs
+++ Editor/Scripts/Interactivity/VisualScriptingExport/InteractivityExportCallback.cs
@@ -19,6 +19,24 @@ namespace UnityGLTF.Interactivity.VisualScripting
             newNode.Index = nodes.Count - 1;
             return newNode;
         }
+
+        public GltfInteractivityExportNode AddLog(LogHelper.LogLevel level, string messageTemplate)
+        {
+            string messagePrefix = "";
+            switch (level)
+            {
+                case LogHelper.LogLevel.Warning:
+                    messagePrefix = "Warning: ";
+                    break;
+                case LogHelper.LogLevel.Error:
+                    messagePrefix = "Error: ";
+                    break;
+            }
+            
+            var gltf_Node = CreateNode(new Debug_LogNode());
+            gltf_Node.Configuration[Debug_LogNode.IdConfigMessage].Value = messagePrefix + messageTemplate;
+            return gltf_Node;
+        }
         
         internal GltfInteractivityExportNodes(List<GltfInteractivityExportNode> nodes)
         {
