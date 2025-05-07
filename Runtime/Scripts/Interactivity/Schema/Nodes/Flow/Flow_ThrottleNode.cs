8d1272685890b1e3aa9531f7884764fd53bd3dd1
Robert Dorn <dorn@prefrontalcortex.de>
2025-05-07 14:51:02 +0200

    Interactivity: added flow/throttle schema

---
+++ Runtime/Scripts/Interactivity/Schema/Nodes/Flow/Flow_ThrottleNode.cs
@@ -0,0 +1,23 @@
+namespace UnityGLTF.Interactivity.Schema
+{
+    public class Flow_ThrottleNode : GltfInteractivityNodeSchema
+    {
+        public override string Op { get; set; } = "flow/throttle";
+
+        [FlowInSocketDescription]
+        public const string IdFlowIn = "in";
+        [FlowInSocketDescription]
+        public const string IdFlowreset = "reset";
+        
+        [InputSocketDescription]
+        public const string IdInputDuration = "duration";
+        
+        [FlowOutSocketDescription]
+        public const string IdFlowOut = "out";
+        [FlowOutSocketDescription]
+        public const string IdFlowOutError = "err";
+        
+        [OutputSocketDescription(GltfTypes.Float)]
+        public const string IdOutElapsedTime = "lastRemainingTime";
+    }
+}
