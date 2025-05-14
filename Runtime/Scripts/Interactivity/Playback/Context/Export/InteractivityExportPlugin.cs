14ee37f1e5fffdb3562c3c71851fac638727e256
Jordan Robinson <thefriendgineer@gmail.com>
2025-05-14 06:13:59 -0400

    Changed plugin names.

--- Runtime/Scripts/Interactivity/Playback/Context/Export/InteractivityExportPlugin.cs
+++ Runtime/Scripts/Interactivity/Playback/Context/Export/InteractivityExportPlugin.cs
@@ -4,8 +4,8 @@ namespace UnityGLTF.Interactivity.Playback
 {
     public class InteractivityExportPlugin : GLTFExportPlugin
     {
-        public override string DisplayName => "KHR_interactivity_Exporter";
-        public override string Description => "Exports KHR compliant interactivity graphs";
+        public override string DisplayName => "KHR_interactivity Playback Exporter";
+        public override string Description => "Allows the export of KHR compliant interactivity graphs at runtime.";
 
         private InteractivityExportContext _context;
         public readonly KHR_interactivity extensionData;
