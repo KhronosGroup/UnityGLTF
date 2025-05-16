ced9c80053a5d184d91798526ebfbd7f6e8f881d
Robert Dorn <dorn@prefrontalcortex.de>
2025-05-16 17:14:32 +0200

    Interactivity: FLow_ForLoop Schema > added default value for initialIndex config

--- Runtime/Scripts/Interactivity/Schema/Nodes/Flow/Flow_ForLoopNode.cs
+++ Runtime/Scripts/Interactivity/Schema/Nodes/Flow/Flow_ForLoopNode.cs
@@ -18,7 +18,7 @@ namespace UnityGLTF.Interactivity.Schema
         public const string IdEndIndex = "endIndex";
         [OutputSocketDescription(GltfTypes.Int)]
         public const string IdIndex = "index";
-        [ConfigDescription]
+        [ConfigDescription(0)]
         public const string IdConfigInitialIndex = "initialIndex";
     }
 }
