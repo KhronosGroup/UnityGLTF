667b788af55d3c94e1f40e864de887b2c1b7dd86
hybridherbst <felix.herbst@gmail.com>
2025-05-20 10:52:03 +0200

    add Sprite Renderer bake-to-mesh export

---
+++ Runtime/Scripts/Plugins/Experimental/SpriteRendererExport.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Linq;
+using GLTF.Schema;
+using UnityEngine;
+
+namespace UnityGLTF.Plugins
+{
+    [ExperimentalPlugin]
+    class SpriteRendererExport : GLTFExportPlugin
+    {
+        public override string DisplayName => "Bake to Mesh: Sprites";
+
+        public override GLTFExportPluginContext CreateInstance(ExportContext context)
+        {
+            return new Context();
+        }
+
+        class Context : GLTFExportPluginContext
+        {
+            readonly List<GameObject> _meshes = new List<GameObject>();
+            public override void BeforeNodeExport(GLTFSceneExporter exporter, GLTFRoot gltfRoot, Transform transform, Node node)
+            {
+                var renderer = transform.GetComponent<SpriteRenderer>();
+                if (!renderer) return;
+
+                var sprite = renderer.sprite;
+                if (!sprite) return;
+
+                var texture = sprite.texture;
+                var verts = sprite.vertices;
+                var tris = sprite.triangles;
+                var uvs = sprite.uv;
+
+                var mesh = new Mesh();
+                mesh.vertices = verts.Select(v => new Vector3(v.x, v.y, 0)).ToArray();
+                mesh.triangles = tris.Select(t => (int) t).ToArray();
+                mesh.uv = uvs;
+
+                var unlitMat = new Material(Shader.Find("UnityGLTF/UnlitGraph"));
+                unlitMat.SetTexture("baseColorTexture", texture);
+                unlitMat.SetColor("baseColorFactor", renderer.color.linear);
+                UnlitGraphMap map = new UnlitGraphMap(unlitMat);
+                map.AlphaMode = AlphaMode.BLEND;
+
+                var go = new GameObject("mesh");
+                go.AddComponent<MeshFilter>().sharedMesh = mesh;
+                go.AddComponent<MeshRenderer>().sharedMaterial = unlitMat;
+                go.transform.SetParent(transform, false);
+
+                _meshes.Add(go);
+            }
+
+            public override void AfterSceneExport(GLTFSceneExporter exporter, GLTFRoot gltfRoot)
+            {
+                foreach (var mesh in _meshes)
+                {
+                    Destroy(mesh);
+                }
+            }
+        }
+    }
+}
