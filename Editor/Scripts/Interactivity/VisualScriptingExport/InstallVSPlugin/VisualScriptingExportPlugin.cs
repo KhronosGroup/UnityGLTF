5bd1094d3fc488e8aa0bb41ae01216d5f7aa4574
Robert Dorn <dorn@prefrontalcortex.de>
2025-05-20 10:43:55 +0200

    Interactivity: added missing-VS-package Plugin

---
+++ Editor/Scripts/Interactivity/VisualScriptingExport/InstallVSPlugin/VisualScriptingExportPlugin.cs
@@ -0,0 +1,38 @@
+#if !HAVE_VISUAL_SCRIPTING
+
+using System;
+using UnityEngine;
+using UnityGLTF.Interactivity.Schema;
+
+namespace UnityGLTF.Interactivity.VisualScripting
+{
+    using UnityGLTF;
+    using UnityGLTF.Plugins;
+
+    /// <summary>
+    /// Extends UnityGLTF with an extension for KHR_Interactivity Metadata
+    ///
+    /// See https://github.com/KhronosGroup/UnityGLTF?tab=readme-ov-file#extensibility
+    /// for the external documentation on how to extend UnityGLTF.
+    /// </summary>
+    [NonRatifiedPlugin]
+    public class VisualScriptingExportPlugin: GLTFExportPlugin
+    {
+        
+        // Disabled by default until Gltf Interactivity spec is ratified
+        public override bool EnabledByDefault => false;
+
+        public override bool Enabled => false;
+        
+        public override string DisplayName => GltfInteractivityExtension.ExtensionName + " (VisualScripting)";
+        public override string Description => "Exports flow graph data for Visual Scripting ScriptMachines.";
+
+        public override string Warning => "Add the package \"com.unity.visualscripting\" to your project.";
+
+        public override GLTFExportPluginContext CreateInstance(ExportContext context)
+        {
+            return null;
+        }
+    }
+}
+#endif
