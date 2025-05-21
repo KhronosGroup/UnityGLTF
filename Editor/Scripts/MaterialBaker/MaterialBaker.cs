121626cd3a356928301bb70053b76e8587e8b8dd
Robert Dorn <dorn@prefrontalcortex.de>
2025-05-21 16:29:44 +0200

    added bake render mesh method (WIP)

--- Editor/Scripts/MaterialBaker/MaterialBaker.cs
+++ Editor/Scripts/MaterialBaker/MaterialBaker.cs
@@ -38,6 +38,66 @@ namespace UnityGLTF
         }
     
 #if HAVE_URP
+        
+        public static Texture2D Bake(Renderer renderer, DebugMaterialMode mode)
+        {
+            // TODO: submeshes
+            var mesh = renderer.GetComponent<MeshFilter>().sharedMesh;
+            
+            var rt = RenderTexture.GetTemporary(1024, 1024, 0, RenderTextureFormat.ARGB32);
+            var material = Object.Instantiate(renderer.sharedMaterial);
+        
+            var patchedShader = ShaderModifier.PatchShaderUVsToClipSpace(material.shader);
+            material.shader = patchedShader;
+          
+            var cmd = new CommandBuffer();
+            cmd.SetRenderTarget(rt);
+            cmd.ClearRenderTarget(true, true, Color.blue);
+            
+            // Set cmd.SetViewProjectionMatrices to get the correct view projection matrix to the mesh
+            
+            cmd.EnableKeyword(new GlobalKeyword(ShaderKeywordStrings.DEBUG_DISPLAY));
+            cmd.SetGlobalFloat("_DebugMaterialMode", (int)mode);
+            cmd.DrawMesh(mesh, Matrix4x4.identity, material, 0, 0);
+            Graphics.ExecuteCommandBuffer(cmd);
+            //
+            // cmd.Clear();
+            // cmd.DisableKeyword(new GlobalKeyword(ShaderKeywordStrings.DEBUG_DISPLAY));
+            // Graphics.ExecuteCommandBuffer(cmd);
+            //
+      
+            
+           //  Graphics.SetRenderTarget(rt);
+           //  var rp = new RenderParams(material);
+           //  rp.receiveShadows = false;
+           //  rp.shadowCastingMode = ShadowCastingMode.Off;
+           //  
+           //  Shader.EnableKeyword(ShaderKeywordStrings.DEBUG_DISPLAY);
+           //  Shader.SetGlobalFloat("_DebugMaterialMode", (int)mode);
+           //  
+           // // Graphics.RenderMesh(rp, renderer.GetComponent<MeshFilter>().sharedMesh, 0, Matrix4x4.identity);
+           //  material.SetPass(0);
+           //  Graphics.DrawMeshNow( renderer.GetComponent<MeshFilter>().sharedMesh, Matrix4x4.identity, 0);
+           //
+            RenderTexture.active = rt;
+            var bakedTexture = new Texture2D(1024, 1024, TextureFormat.RGBA32, false);
+            bakedTexture.wrapMode = TextureWrapMode.Repeat;
+            bakedTexture.filterMode = FilterMode.Bilinear;
+            bakedTexture.anisoLevel = 1;
+            bakedTexture.Apply();
+            // Read pixels from renderTexture and apply to bakedTexture
+            bakedTexture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+      
+            RenderTexture.active = null;
+            RenderTexture.ReleaseTemporary(rt);
+
+            
+            Shader.DisableKeyword(ShaderKeywordStrings.DEBUG_DISPLAY);
+
+            return bakedTexture;
+            
+
+        }
 
         private static void DeactivateGlobalUrpDebugProperties()
         {
