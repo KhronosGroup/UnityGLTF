75a184448c4065075738fc05209c37a4f1ed254a
Robert Dorn <dorn@prefrontalcortex.de>
2025-06-02 20:25:34 +0200

    Interactivity: added special case to GenericMulitplyUnitExporter > Quat * V3 (math/rotate3d)

--- Editor/Scripts/Interactivity/VisualScriptingExport/UnitExporters/Math/GenericMultiplyUnitExporter.cs
+++ Editor/Scripts/Interactivity/VisualScriptingExport/UnitExporters/Math/GenericMultiplyUnitExporter.cs
@@ -25,11 +25,22 @@ namespace UnityGLTF.Interactivity.VisualScripting.Export
             mulNode.FirstValueOut().MapToPort(unit.product);
 
             // We need to check later - when its available - the input type of the node,
-            // for Matrix4x4 we need to change the Schema to math/matmul
             unitExporter.vsExportContext.OnUnitNodesCreated += nodes =>
             {
-                var valueType = unitExporter.vsExportContext.GetValueTypeForInput(mulNode, "a");
-                if (valueType == GltfTypes.TypeIndexByGltfSignature("float4x4"))
+                var valueTypeA = unitExporter.vsExportContext.GetValueTypeForInput(mulNode, "a");
+                var valueTypeB = unitExporter.vsExportContext.GetValueTypeForInput(mulNode, "b");
+                // for Quat * V3 we need to change the Schema to math/rotate3D
+                if (valueTypeA == GltfTypes.TypeIndexByGltfSignature("float4")
+                    && valueTypeB == GltfTypes.TypeIndexByGltfSignature("float3"))
+                {
+                    mulNode.SetSchema(new Math_Rotate3dNode(), false);
+                    mulNode.ValueIn("a").SetType(TypeRestriction.LimitToFloat4);
+                    mulNode.ValueIn("b").SetType(TypeRestriction.LimitToFloat3);
+                    mulNode.FirstValueOut().ExpectedType(ExpectedType.Float3);
+                }
+                else
+                // for Matrix4x4 we need to change the Schema to math/matmul
+                if (valueTypeB == GltfTypes.TypeIndexByGltfSignature("float4x4"))
                 {
                     mulNode.SetSchema(new Math_MatMulNode(), false);
                     mulNode.ValueIn("a").SetType(TypeRestriction.LimitToFloat4x4);
