4d47bf0ee90d6b1dae8643e75678aca68d6c7516
Jordan Robinson <thefriendgineer@gmail.com>
2025-06-04 13:49:15 -0400

    Updated math/rotate3D to match new spec changes. Updated name of math/rotate2D to capitalize the D.

--- Runtime/Scripts/Interactivity/Playback/Nodes/Math/Rotate3D.cs
+++ Runtime/Scripts/Interactivity/Playback/Nodes/Math/Rotate3D.cs
@@ -1,6 +1,7 @@
 using System;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityGLTF.Interactivity.Playback.Extensions;
 
 namespace UnityGLTF.Interactivity.Playback
 {
@@ -14,19 +15,21 @@ namespace UnityGLTF.Interactivity.Playback
         {
             TryEvaluateValue(ConstStrings.A, out IProperty a);
             TryEvaluateValue(ConstStrings.B, out IProperty b);
-            TryEvaluateValue(ConstStrings.C, out IProperty c);
 
             return a switch
             {
-                Property<float3> aProp when b is Property<float3> bProp && c is Property<float> cProp => new Property<float3>(rotate(aProp.value, bProp.value, cProp.value)),
+                Property<float3> aProp when b is Property<float4> bProp => new Property<float3>(rotate(aProp.value, bProp.value)),
                 _ => throw new InvalidOperationException("No supported type found."),
             };
         }
 
-        private static float3 rotate(float3 vec, float3 axis, float rad)
+        private static float3 rotate(float3 a, float4 b)
         {
-            // TODO: Test rotation direction to make sure it matches the spec (counter-clockwise).
-            return math.mul(quaternion.AxisAngle(axis, rad), vec);
+            var b3 = b.xyz;
+            var b3xa = math.cross(b3, a);
+            // Is this equivalent to math.mul(b.ToQuaternion(), a)?
+            // I have no idea so let's be safe and use the eq from the spec.
+            return a + 2f * (math.cross(b3, b3xa) + b.w * b3xa);
         }
     }
 }
