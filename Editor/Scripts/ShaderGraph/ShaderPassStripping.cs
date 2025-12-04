be7169f12b478e35ac9548af9960e2a83b7dfc10
Robert Dorn <mich@robertdorn.de>
2025-12-04 14:24:38 +0100

    changed ShaderPassStripping requesting GLTFSettings to GetOrCreate

--- Editor/Scripts/ShaderGraph/ShaderPassStripping.cs
+++ Editor/Scripts/ShaderGraph/ShaderPassStripping.cs
@@ -33,8 +33,7 @@ namespace UnityGLTF
         
         public ShaderPassStripping()
         {
-            if (GLTFSettings.TryGetSettings(out var s))
-                settings = s.shaderStrippingSettings;
+            settings = GLTFSettings.GetOrCreateSettings()?.shaderStrippingSettings;
         }
 
         private bool ShouldStripPass(ShaderSnippetData snippet)
