e8f2e2563accc64f0a62b298fe13456511093e1d
Robert Dorn <mich@robertdorn.de>
2026-04-23 14:52:34 +0200

    added log for missing references in gltf export context

--- Runtime/Scripts/Interactivity/Schema/RefResolver.cs
+++ Runtime/Scripts/Interactivity/Schema/RefResolver.cs
@@ -11,24 +11,38 @@ namespace Interactivity.Schema
             if (value == null)
                 return false;
 
+            void LogMissingError()
+            {
+                Debug.LogError($"Could not find a reference for object {value} of type {value.GetType()} in the exported glTF scene. This object will not be interactable in the exported glTF.", value as Object);
+            }
+            
             switch (value)
             {
                 case GameObject go:
                     var goIndex = sceneExporter.GetTransformIndex(go.transform);
                     if (goIndex == -1)
+                    {
+                        LogMissingError();
                         return false;
+                    }
                     staticJson = $"/nodes/{goIndex}/";
                     return true;
                 case Transform tr:
                     var transformIndex = sceneExporter.GetTransformIndex(tr);
                     if (transformIndex == -1)
+                    {
+                        LogMissingError();
                         return false;
+                    }
                     staticJson = $"/nodes/{transformIndex}/";
                     return true;
                 case Material mat:
                     var materialIndex = sceneExporter.GetMaterialIndex(mat);
                     if (materialIndex == -1)
+                    {
+                        LogMissingError();
                         return false;
+                    }
                     staticJson = $"/materials/{materialIndex}/";
                     return true;
                 case Mesh mesh:
@@ -38,7 +52,10 @@ namespace Interactivity.Schema
                     var goOfComp = comp.gameObject;
                     var goOfCompIndex = sceneExporter.GetTransformIndex(goOfComp.transform);
                     if (goOfCompIndex == -1)
+                    {
+                        LogMissingError();
                         return false;
+                    }
                     staticJson = $"/nodes/{goOfCompIndex}/";
                     return true;
             }
